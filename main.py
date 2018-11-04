dd24ccf30a68ed412638997639faba99da92b405
Enoch <daneolog@gmail.com>
2018-11-04 09:09:59 -0500

    got ui working

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from tkinter import Tk, Label, Button, Text
+from draw_stats import *
 
-driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
-elem = driver.find_element_by_name("q")
-elem.clear()
-elem.send_keys("pycon")
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
-driver.close()
+class MyFirstGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("A simple GUI")
+
+        self.label = Label(master, text="Please enter a team number:")
+        self.label.pack()
+
+        self.team_number = Text(master)
+        self.team_number.pack()
+
+        self.close_button = Button(master, text="Produce Files", command=lambda: main(self.team_number.get('1.0', 'end-1c')))
+        self.close_button.pack()
+
+root = Tk()
+my_gui = MyFirstGUI(root)
+root.mainloop()
